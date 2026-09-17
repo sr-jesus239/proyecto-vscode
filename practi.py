1ea0a40e0c8644ccb3d4cd8c7144968efbaf2f73
Andreimi Jesus Montero Heredia <linkedlnmonteroheredia@gmail.com>
2026-09-17 16:40:12 -0400

    Update practi.py

--- practi.py
+++ practi.py
@@ -1,8 +1,1 @@
-nombre = input("¿Cuál es tu nombre? ")
-edad = input("¿Cuántos años tienes? ")
 
-print("Hola", nombre)
-print("Tienes", edad, "años")
-
-print("Hola, estoy aprendiendo Python")
-print("Mi archivo está conectado con GitHub")
